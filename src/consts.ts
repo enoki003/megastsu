97ff4b8065ed7eca5e05b23571bb80dd64ed7f9e
enoki003 <kmd003@outlook.com>
2026-02-23 23:41:37 +0900

    サイトタイトルと説明を日本語に更新し、コンテンツを日本語に翻訳

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '園木優陽　ホームページ';
+export const SITE_DESCRIPTION = '園木優陽のホームページです。日々の活動を記録しています。';
